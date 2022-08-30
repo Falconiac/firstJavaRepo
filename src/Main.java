0b0608978a88ee1323dd8eea53637f506ba44ba7
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-08-30 16:34:04 +0200

    This is the second commit...

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,5 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First Commit in new Repo");
-    }
+        System.out.println("Second commit change...");
 }
