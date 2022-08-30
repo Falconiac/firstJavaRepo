f6400d0a85e96441bdca6d70e20721266eb6767b
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-08-30 16:40:58 +0200

    Second commit on first-feature-branch

--- src/firstFeature.java
+++ src/firstFeature.java
@@ -3,5 +3,6 @@ package src;
 public class firstFeature {
     public static void main(String[] args) {
         System.out.println("first feature");
+        System.out.println("second commit on first-feature-branch");
     }
 }
