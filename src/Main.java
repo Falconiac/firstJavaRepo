2789279f2fe5f8e11ef2a74e0a1c0c41aa78bcef
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-08-30 17:46:10 +0200

    Fixed bug

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First Commit in new Repo");
         System.out.println("Second commit change...");
+    }
 }
